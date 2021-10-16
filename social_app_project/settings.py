46c74b41aa26fa5434fc48bbc2c80a724474e0e0
M Farhan Zia <farhan71727@gmail.com>
2021-10-16 19:07:30 +0500

    env created

--- social_app_project/settings.py
+++ social_app_project/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-y^=0a^iz^@sk37bcuk_a!k&f+_&jxph!#vg%)bfkt#4hep)v(q'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
